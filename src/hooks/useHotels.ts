25426a0218671fa8b11a9f2b7c7e8427d5381823
hyebinjo <hyebinjo91@gmail.com>
2022-08-01 23:38:10 +0900

    feat: setTimeout 500ms 적용

--- src/hooks/useHotels.ts
+++ src/hooks/useHotels.ts
@@ -9,20 +9,24 @@ export default function useHotels() {
 
   async function getAllByPage(page: number = 1) {
     setIsLoading(true);
-    const data = await hotelsService.get(`?_page=${page}`);
-    page === 1 ? setHotels(data) : setHotels([...hotels, ...data]);
-    setIsLoading(false);
+    setTimeout(async () => {
+      const data = await hotelsService.get(`?_page=${page}`);
+      page === 1 ? setHotels(data) : setHotels([...hotels, ...data]);
+      setIsLoading(false);
+    }, 500);
   }
 
   async function getResultsByPage(searchParameter: UserDataType, page: number = 1) {
     setIsLoading(true);
     const searchString = searchParameter.hotelName?.split(' ').join('+') || '';
     const neQueryString = getExceptedHotelsQueryString(searchParameter.checkInDate, searchParameter.checkOutDate);
-    const data = await hotelsService.get(
-      `?occupancy.max_gte=${searchParameter.numberOfGuests}&q=${searchString}${neQueryString}&_page=${page}`,
-    );
-    page === 1 ? setHotels(data) : setHotels([...hotels, ...data]);
-    setIsLoading(false);
+    setTimeout(async () => {
+      const data = await hotelsService.get(
+        `?occupancy.max_gte=${searchParameter.numberOfGuests}&q=${searchString}${neQueryString}&_page=${page}`,
+      );
+      page === 1 ? setHotels(data) : setHotels([...hotels, ...data]);
+      setIsLoading(false);
+    }, 500);
   }
 
   return { isLoading, hotels, getAllByPage, getResultsByPage };
