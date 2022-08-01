29d79c6ef072ef4b3961316ce7236e30a5e11dbd
hyebinjo <hyebinjo91@gmail.com>
2022-08-01 17:44:10 +0900

    feat: getResultsByPage함수(not equal 적용 전)

--- src/hooks/useHotels.ts
+++ src/hooks/useHotels.ts
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { hotelsService } from '../api/axiosInstance';
-import { Hotel } from '../interfaces/types';
+import { Hotel, UserDataType } from '../interfaces/types';
 
 export default function useHotels() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [hotels, setHotels] = useState<Hotel[]>([]);
 
   async function getAllByPage(page: number = 1) {
@@ -13,5 +13,15 @@ export default function useHotels() {
     setIsLoading(false);
   }
 
-  return { isLoading, hotels, getAllByPage };
+  async function getResultsByPage(searchParameter: UserDataType, page: number = 1) {
+    setIsLoading(true);
+    const searchString = searchParameter.hotelName?.split(' ').join('+') || '';
+    const data = await hotelsService.get(
+      `?occupancy.max_gte=${searchParameter.numberOfGuests}&q=${searchString}&_page=${page}`,
+    );
+    page === 1 ? setHotels(data) : setHotels([...hotels, ...data]);
+    setIsLoading(false);
+  }
+
+  return { isLoading, hotels, getAllByPage, getResultsByPage };
 }
