cc79ffc70c033006a0574e60321dfdcd2a2772f5
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-01 18:07:25 +0900

    feat: Option Selector 추가
    인원 수(성인, 어린이)를 선택하는 선택 컴포넌트

--- src/components/searchBar/Index.tsx
+++ src/components/searchBar/Index.tsx
@@ -3,15 +3,19 @@ import styled from 'styled-components';
 import SearchIcon from '../../assets/search';
 import { theme } from '../../styles/theme';
 import InputSearch from './InputSearch';
+import OptionSelector from './OptionSelector';
 
 export default function SearchBar() {
   const [isFocusInput, setIsFocusInput] = useState(false);
+
+  const [numberOfPeople, setNumberOfPeople] = useState({ adult: 2, children: 0 });
+
   return (
     <Container>
       <Wrapper>
         <InputSearch isFocusInput={isFocusInput} setIsFocusInput={setIsFocusInput} />
         <Datepicker />
-        <OptionSelector></OptionSelector>
+        <OptionSelector numberOfPeople={numberOfPeople} setNumberOfPeople={setNumberOfPeople} />
         <Button>
           <SearchIcon />
         </Button>
@@ -23,15 +27,16 @@ export default function SearchBar() {
 const Container = styled.div`
   padding: 1rem 0;
   border-bottom: 1px solid ${theme.borderColor};
+  width: 100%;
 `;
 const Wrapper = styled.div`
-  width: 976px;
+  max-width: 976px;
+  width: 100%;
   margin: 0 auto;
   height: 60px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  overflow: hidden;
   border: 1px solid ${theme.borderColor};
   border-radius: 4px;
 `;
@@ -41,10 +46,7 @@ const Datepicker = styled.div`
   width: 100%;
   border-right: 1px solid ${theme.borderColor};
 `;
-const OptionSelector = styled.div`
-  height: 100%;
-  width: 100%;
-`;
+
 const Button = styled.button`
   border: none;
   background-color: ${theme.primaryColor};
---
+++ src/components/searchBar/OptionSelector.tsx
@@ -0,0 +1,157 @@
+import styled from 'styled-components';
+import UserSingle from '../../assets/UserSingle';
+import { theme } from '../../styles/theme';
+
+interface NumberOfPeople {
+  adult: number;
+  children: number;
+}
+
+interface Container {
+  numberOfPeople: NumberOfPeople;
+  setNumberOfPeople: React.Dispatch<React.SetStateAction<NumberOfPeople>>;
+}
+
+export default function OptionSelector({ numberOfPeople, setNumberOfPeople }: Container) {
+  const isLessThen = (number: number, lessThen: number) => number < lessThen;
+  const isMoreThen = (number: number, moreThen: number) => number > moreThen;
+
+  const MAXIMUM = {
+    adult: 8,
+    children: 8,
+  };
+  const MINIMUM = {
+    adult: 1,
+    children: 0,
+  };
+
+  const adultIncrease = () => {
+    if (isMoreThen(numberOfPeople.adult + 1, MAXIMUM.adult)) return;
+    setNumberOfPeople((prevState) => ({ ...prevState, adult: prevState.adult + 1 }));
+  };
+  const childrenIncrease = () => {
+    if (isMoreThen(numberOfPeople.children + 1, MAXIMUM.children)) return;
+    setNumberOfPeople((prevState) => ({ ...prevState, children: prevState.children + 1 }));
+  };
+  const adultDecrease = () => {
+    if (isLessThen(numberOfPeople.adult - 1, MINIMUM.adult)) return;
+    setNumberOfPeople((prevState) => ({ ...prevState, adult: prevState.adult - 1 }));
+  };
+  const childrenDecrease = () => {
+    if (isLessThen(numberOfPeople.children - 1, MINIMUM.children)) return;
+    setNumberOfPeople((prevState) => ({ ...prevState, children: prevState.children - 1 }));
+  };
+
+  return (
+    <Container>
+      <Column>
+        <UserSingle width="1.5rem" height="1.5rem" />
+      </Column>
+      <Column>
+        <Span styledColor={theme.fontLightColor} thin={true} smallFont={true}>
+          인원
+        </Span>
+        <Span>{numberOfPeople.adult + numberOfPeople.children} 명</Span>
+      </Column>
+      <Counter>
+        <Row>
+          <Label>성인</Label>
+          <CountWrapper>
+            <MinusButton disabled={numberOfPeople.adult === MINIMUM.adult} onClick={adultDecrease} />
+            <Count>{numberOfPeople.adult}</Count>
+            <PlusButton disabled={numberOfPeople.adult === MAXIMUM.adult} onClick={adultIncrease} />
+          </CountWrapper>
+        </Row>
+        <Row>
+          <Label>아이</Label>
+          <CountWrapper>
+            <MinusButton disabled={numberOfPeople.children === MINIMUM.children} onClick={childrenDecrease} />
+            <Count>{numberOfPeople.children}</Count>
+            <PlusButton disabled={numberOfPeople.children === MAXIMUM.children} onClick={childrenIncrease} />
+          </CountWrapper>
+        </Row>
+      </Counter>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  padding: 1rem;
+  gap: 1rem;
+`;
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  justify-content: center;
+`;
+const Span = styled.span<{ styledColor?: string; thin?: boolean; smallFont?: boolean }>`
+  ${(props) => props.styledColor && `color:${props.styledColor}`};
+  ${(props) => props.thin && `font-weight:300`};
+  ${(props) => props.smallFont && `font-size: 0.7rem`};
+`;
+
+const Counter = styled.div`
+  position: absolute;
+  width: 360px;
+  top: 66px;
+  right: 4px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.3);
+  background-color: white;
+  padding: 1rem;
+  border-radius: 4px;
+  gap: 1.5rem;
+`;
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const CountWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Label = styled.span`
+  font-size: 0.9rem;
+  color: ${theme.fontMediumColor};
+  font-weight: 200;
+`;
+const Count = styled.span`
+  font-size: 1.2rem;
+  padding: 0 1rem;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  border: 1px solid rgb(104, 104, 136);
+  border-radius: 4px;
+  width: 1.375rem;
+  height: 1.375rem;
+  :disabled {
+    border: 1px solid ${theme.borderColor};
+    pointer-events: none;
+  }
+`;
+const MinusButton = styled(Button)`
+  background: rgba(0, 0, 0, 0)
+    url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI4IiBoZWlnaHQ9IjEiIHZpZXdCb3g9IjAgMCA4IDEiPjxyZWN0IHdpZHRoPSI4IiBoZWlnaHQ9IjEiIGZpbGw9IiM2ODY4ODgiIGZpbGwtcnVsZT0iZXZlbm9kZCIgcng9Ii41Ii8+PC9zdmc+')
+    no-repeat scroll center center / 8px;
+  :disabled {
+    background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI4IiBoZWlnaHQ9IjEiIHZpZXdCb3g9IjAgMCA4IDEiPjxyZWN0IHdpZHRoPSI4IiBoZWlnaHQ9IjEiIGZpbGw9IiNDRENEQ0QiIGZpbGwtcnVsZT0iZXZlbm9kZCIgcng9Ii41Ii8+PC9zdmc+');
+  }
+`;
+
+const PlusButton = styled(Button)`
+  background: rgba(0, 0, 0, 0)
+    url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI4IiBoZWlnaHQ9IjgiIHZpZXdCb3g9IjAgMCA4IDgiPjxwYXRoIGZpbGw9IiM2ODY4ODgiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTQgMGEuNS41IDAgMDEuNS41djNoM2EuNS41IDAgMDEwIDFINC40OTlsLjAwMSAzYS41LjUgMCAwMS0xIDBsLS4wMDEtM0guNWEuNS41IDAgMDEwLTFoM3YtM0EuNS41IDAgMDE0IDB6Ii8+PC9zdmc+')
+    no-repeat scroll center center / 8px;
+  :disabled {
+    background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI4IiBoZWlnaHQ9IjgiIHZpZXdCb3g9IjAgMCA4IDgiPjxnIGZpbGw9IiNDRENEQ0QiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHJlY3Qgd2lkdGg9IjgiIGhlaWdodD0iMSIgeT0iMy41IiByeD0iLjUiLz48cmVjdCB3aWR0aD0iOCIgaGVpZ2h0PSIxIiB5PSIzLjUiIHJ4PSIuNSIgdHJhbnNmb3JtPSJyb3RhdGUoOTAgNCA0KSIvPjwvZz48L3N2Zz4=');
+  }
+`;
