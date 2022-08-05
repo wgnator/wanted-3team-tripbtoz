f5c7b67d765a5bc5ed644e5f8d07e4ddf5b7111f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-05 12:52:27 +0900

    fix: 디테일 페이지 서치바 표시

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,11 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useMatch } from 'react-router-dom';
 import Header from './Header';
 import SearchBar from './searchBar/Index';
 
 export default function Layout() {
   const locate = useLocation();
-  const hasSearchBar = locate.pathname === '/detail' || locate.pathname === '/';
+  const hasSearchBar =
+    locate.pathname === '/detail' || locate.pathname === '/' || useMatch({ path: '/details/:hotelName' });
 
   return (
     <>
