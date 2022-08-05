1fb88f19a02dd80a9fba8fd562e402fc81ca0331
hyebinjo <hyebinjo91@gmail.com>
2022-08-05 12:59:39 +0900

    fix: 에러 수정

--- src/utils/getQueryString.ts
+++ src/utils/getQueryString.ts
@@ -1,12 +1,12 @@
 import areIntervalsOverlapping from 'date-fns/areIntervalsOverlapping';
-import { UserDataType, UserData } from '../interfaces/types';
+import { UserDataType, UserData, searchQueryType } from '../interfaces/types';
 
-function getExceptedHotelsQueryString(searchParameter: UserDataType, userHotels: UserData): string {
+function getExceptedHotelsQueryString(searchParameter: searchQueryType, userHotels: UserData): string {
   const exceptedHotels = userHotels
     .filter((reservation: UserDataType) =>
       areIntervalsOverlapping(
         { start: new Date(reservation.checkInDate), end: new Date(reservation.checkOutDate) },
-        { start: searchParameter.checkInDate, end: searchParameter.checkOutDate },
+        { start: new Date(searchParameter.checkInDate), end: new Date(searchParameter.checkOutDate) },
       ),
     )
     .map((reservation: UserDataType) => reservation.hotelName);
@@ -14,7 +14,7 @@ function getExceptedHotelsQueryString(searchParameter: UserDataType, userHotels:
   return neQueryString;
 }
 
-export function getSearchQueryString(searchParameter: UserDataType, userHotels: UserData): string {
+export function getSearchQueryString(searchParameter: searchQueryType, userHotels: UserData): string {
   const searchString = searchParameter.hotelName?.split(' ').join('+') || '';
   const neQueryString = getExceptedHotelsQueryString(searchParameter, userHotels);
   const searchQueyrString = `occupancy.max_gte=${searchParameter.numberOfGuests}&q=${searchString}${neQueryString}`;
